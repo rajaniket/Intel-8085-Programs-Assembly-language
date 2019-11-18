c1227273669ead8c389c9d8162d5986af86d2ede
Aniket Raj <aniketraj.go@gmail.com>
2019-11-18 23:28:10 +0530

    add 10 bytes of data stored in memory address

--- q6_add_of_10_bytes.asm
+++ q6_add_of_10_bytes.asm
@@ -1,6 +1,7 @@
-;add 10 bytes of data stored in mem add 0030;
-;store result in 0040 and addtion carries in 0041
- mvi c,0Ah ;intializing counter value
+;add 10 bytes of data stored in memory address 0030 to 0039
+;store the result in 0040 and addtion carries in 0041
+
+mvi c,0Ah ;intializing counter value
 mvi a,00h
 mvi b,00h ;for carry
 lxi h,0030h
