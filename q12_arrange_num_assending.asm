b6ca153e9faa5cd06aba0e849c5b2e5e4517087d
Aniket Raj <aniketraj.go@gmail.com>
2019-11-22 00:20:33 +0530

    arrange the numbers in ascending order

--- q12_arrange_num_assending.asm
+++ q12_arrange_num_assending.asm
@@ -1,20 +1,20 @@
-;Arrange the number stores in the memory into assending order 
+;Arrange the number stores in the memory(0030h to 0039h) into ascending order 
 ;
-mvi c,0ah
-dcr c
-mov b,c
-l3: lxi h,0030h
-l2: mov a,m
-inx h
-cmp m
-jc l1
-mov d,m
-mov m,a
-dcx h
-mov m,d
-inx h
-l1: dcr b
-jnz l2
-dcr c
+mvi c,0ah     ; initializing counter value
+dcr c         ; decreasing the counter because it will count 4 ---> 0 =>5 ,when c=0 assending work completed
+l3: mov b,c   ; b is also a counter in case of innner loop 
+lxi h,0030h   ; points to first value
+l2: mov a,m   ; moving 1st value to acc because we want to compare it with next number 
+inx h         ; now 'M' will point next data (e.g. 0031h)
+cmp m         ; comparing a with m (a-m)
+jc l1         ; if m ig greater then jump to l1 means there is no need of exchange (e.g. 0030h<0031h)
+mov d,m       ; now saving the pointed value to reg d so that it remains safe (not get replaced) e.g. d <---- 0031h
+mov m,a       ; after saving m value we replace it with acc data means indirectly (0031h <------ 0030h)
+dcx h         ; decreamented reg h, means now it will point m (m=>m-1)   e.g. h=0030h
+mov m,d       ; m <---- d (d had the data of 0031h,now it is transferd to 0030h)   
+inx h         ;now m is back to its original h=0031h
+l1: dcr b     ; b-1
+jnz l2        ;
+dcr c      
 jnz l3
 hlt
