2f9fe656b2a37e04c52ee432961226df7329df8c
Aniket Raj <aniketraj.go@gmail.com>
2019-11-22 00:26:37 +0530

    Arrange the data into descending order

--- q13_q12_arrange_num_desceding.asm
+++ q13_q12_arrange_num_desceding.asm
@@ -1,5 +1,5 @@
-;Arrange the number stores in the memory into assending order
-;
+;Arrange the number stores in the memory into dscending order
+
 mvi c,0ah
 dcr c
 mov b,c
@@ -7,7 +7,7 @@ l3: lxi h,0030h
 l2: mov a,m
 inx h
 cmp m
-jc l1
+jnc l1
 mov d,m
 mov m,a
 dcx h
