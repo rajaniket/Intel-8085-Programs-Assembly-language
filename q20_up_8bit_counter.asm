d29067286dc590efb2498f724905d2cb7b4bdf91
Aniket Raj <aniketraj.go@gmail.com>
2019-12-05 01:24:31 +0530

    Add files via upload

---
+++ q20_up_8bit_counter.asm
@@ -0,0 +1,20 @@
+;up counter(8 led) with delay of 0.25 sec with ppi 8255
+lxi sp,0040h
+mvi a,80h
+out 13h
+l1: mvi b,00h
+l2: mov a,b
+out 10h
+call delay
+inr b
+mov a,b
+cpi 255
+jnz l2
+jmp l1
+
+delay: lxi h,7a0fh
+k1: dcx h
+mov a,l
+ora h
+jnz k1
+ret
