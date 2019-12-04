1286a56ea022e14464af734065102a7ebff9d8c3
Aniket Raj <aniketraj.go@gmail.com>
2019-12-04 11:13:47 +0530

    Add files via upload

---
+++ q18_square_wave.asm
@@ -0,0 +1,17 @@
+;square wave with delay of 0.25 sec with ppi 8255
+lxi sp,0040h
+mvi a,80h
+out 13h
+l1: mvi a,255
+out 10h
+call delay
+mvi a,00h
+out 10h
+call delay
+jmp l1
+delay: lxi h,7a0fh
+k1: dcx h
+mov a,l
+ora h
+jnz k1
+ret
