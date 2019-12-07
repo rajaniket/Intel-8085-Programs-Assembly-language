d6378f13440fb2df533ff3a1806c2b67bd972ab5
Aniket Raj <aniketraj.go@gmail.com>
2019-12-08 00:05:35 +0530

    Add files via upload

---
+++ q23.asm
@@ -0,0 +1,19 @@
+;prog to add content of portA and PortB
+;store its result in port c
+;use i/o mode of 8255
+lxi sp,100
+mvi a,92h
+out 83h
+in 80
+mov b,a
+in 81
+add b
+out 82
+hlt
+delay: lxi h,18
+l1: dcx h
+mov a,l
+ora h
+jnz l1
+ret
+ 
