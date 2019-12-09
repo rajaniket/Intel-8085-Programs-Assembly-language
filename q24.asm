0388000acc76913fa2113ab58befacb26f1715f9
Aniket Raj <aniketraj.go@gmail.com>
2019-12-09 14:03:57 +0530

    prog to display

--- q24.asm
+++ q24.asm
@@ -1,7 +1,7 @@
-;prog to display 99 in port A
-;1's compliment of 99 in port B
-;2's compliment of 99 in port c
-;use i/o mode of 8255
+;prog to display 99 at port A
+;1's compliment of 99 at port B
+;2's compliment of 99 at port c
+;use i/o mode of ppi 8255
 mvi a,80h
 out 33h
 mvi a,99h
