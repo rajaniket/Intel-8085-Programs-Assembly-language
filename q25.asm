e588b19fa54e9ce9c602f0b266847dc540e6ffc4
Aniket Raj <aniketraj.go@gmail.com>
2019-12-10 22:06:09 +0530

    Add files via upload

---
+++ q25.asm
@@ -0,0 +1,14 @@
+;Take input from port B and c lower
+;And display it from port A and port c lower respectively
+;use i/o mode of 8255
+mvi a,83h
+out 83h
+in 81h
+out 80h
+in 82h
+rlc
+rlc
+rlc
+rlc
+out 82h
+hlt
