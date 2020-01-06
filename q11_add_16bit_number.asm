9d2d297ea210a572be5af52a20806ef678563283
Aniket Raj <aniketraj.go@gmail.com>
2020-01-06 23:56:02 +0530

    q11_add_16bit number

--- q11_add_16bit_number.asm
+++ q11_add_16bit_number.asm
@@ -1,14 +1,15 @@
 ;Add two 16 bit number with each other and store the result in 0035h,0036h and carry into 0038h
 mvi c,00h
-lhld 0030h
-xchg
+lhld 0030h    ;imeediate data transfer to h-l pair 
+xchg          ;exchanging h-l pair data with d-e pair
 lhld 0032h
-dad d
+dad d         ; it is used for 16 bit addition    H-L <---  H-L  +  D-E
 jnc l1
 inr c
-l1: shld 0035h
+l1: shld 0035h   ;saving H-L pair content to 0035h and 0036h
 mov a,c
-sta 0038h
+sta 0038h    ; storing acc content to 0038h
 hlt
 
 
+q11_add_16bit number
