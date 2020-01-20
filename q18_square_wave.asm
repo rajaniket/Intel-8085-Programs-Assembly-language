01d77000b142d1912a45c4964138ad595962580c
Aniket Raj <aniketraj.go@gmail.com>
2020-01-20 23:47:59 +0530

    Update q18_square_wave.asm

--- q18_square_wave.asm
+++ q18_square_wave.asm
@@ -1,17 +1,17 @@
 ; Generate square wave with delay of 0.25 sec use ppi 8255 to take output
-lxi sp,0040h
+lxi sp,0040h  ;initializing stack pointer for subroutine delay
 mvi a,80h
 out 13h
 l1: mvi a,255
 out 10h
-call delay
+call delay    ; delay is defined in bottom
 mvi a,00h
 out 10h
 call delay
 jmp l1
-delay: lxi h,7a0fh
+delay: lxi h,7a0fh    ;at here program counter conent placed at stack
 k1: dcx h
 mov a,l
 ora h
 jnz k1
-ret
+ret                   ;retrieve pc content from stack
