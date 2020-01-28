ed2a17e1b3aa79f9f01ebd947121447fa550d21c
Aniket Raj <aniketraj.go@gmail.com>
2020-01-28 23:31:44 +0530

    Take input from port B and C lower

--- q25.asm
+++ q25.asm
@@ -1,14 +1,14 @@
 ;Take input from port B and c lower
 ;And display it from port A and port c lower bit respectively
 ;use i/o mode of 8255
-mvi a,83h
+mvi a,83h   // Defining control word
 out 83h
 in 81h
-out 80h
+out 80h   // port A
 in 82h
 rlc
 rlc
 rlc
 rlc
-out 82h
+out 82h   // Port c
 hlt
