a74e320dac819387539589e4f95f07ed2107f608
Aniket Raj <aniketraj.go@gmail.com>
2020-02-08 23:54:47 +0530

    prog to display

--- q24.asm
+++ q24.asm
@@ -1,10 +1,10 @@
-;prog to display 99 at port A
-;1's compliment of 99 at port B
-;2's compliment of 99 at port c
+;prog to display 99 at port A of ppi 8255
+;1's compliment of 99 at port B of ppi 8255
+;2's compliment of 99 at port c of ppi 8255
 ;use i/o mode of ppi 8255
-mvi a,80h
-out 33h
-mvi a,99h
+mvi a,80h   ; moving control word
+out 33h     ; contol word executed
+mvi a,99h   
 out 83h
 cma 
 out 31
