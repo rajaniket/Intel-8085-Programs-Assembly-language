1488f3625ee571a309497d09a721c290fb021fd2
Aniket Raj <aniketraj.go@gmail.com>
2020-02-10 21:40:17 +0530

    Compare two numbers and store the smallest no.

--- q5_CMP_MIN.asm
+++ q5_CMP_MIN.asm
@@ -2,11 +2,11 @@
 ;the numbers are stored in mem locn 002h & 0021h
 ;store the smallest number in 0022h
 
-LXI H,0020H
-MOV A,M
-INX H
-CMP M
-JC LABEL
-MOV A,M
-LABEL: STA 0022H
+LXI H,0020H  ;initializing hl pair with 0020h
+MOV A,M      ;moving content at address which is stored in hl pair to accum
+INX H        ;increament hl pair
+CMP M        ;Comparing new content with content at accumulator
+JC LABEL     ; jump if carry  to label 
+MOV A,M      
+LABEL: STA 0022H   ; save acc content to memory location 0022h 
 HLT
