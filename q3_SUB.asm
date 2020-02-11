da5c16f0214f48d0a744c8864967161b9dcca2cb
Aniket Raj <aniketraj.go@gmail.com>
2020-02-11 23:41:26 +0530

    Subtract two number

--- q3_SUB.asm
+++ q3_SUB.asm
@@ -1,8 +1,8 @@
 ;subtract two numbers
 ;numbers are stored in 0020H and 0021H and result in 0022H
-LXI H,0020H
-MOV A,M
-INX H 
-SUB M
-STA 0022H
+LXI H,0020H   ;initializing HL pair with address 0020h
+MOV A,M       ;Moving content of 0020h to acc
+INX H         ;increamenting the address in HL pair
+SUB M         ;Subtracting the pointer value with acc
+STA 0022H     ; saving the result into 0022h
 HLT
