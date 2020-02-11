5e272f198aa06006b4e3cf8377a751b1e554f18f
Aniket Raj <aniketraj.go@gmail.com>
2020-02-11 23:34:43 +0530

    Mov the 15 bytes of data frm one to other mem locn

--- q9_mov_15_bytes_data.asm
+++ q9_mov_15_bytes_data.asm
@@ -1,11 +1,11 @@
 ;move the 15 bytes of data stored in memory location (0030h to 003Eh) into memory location (0040H to 004E) 
-lxi h,0030h
-lxi d,0040h
-mvi c,0fh
-start: mov a,m
-stax d
-inx d
-inx h
-dcr c
-jnz start
+lxi h,0030h   ;initializing HL pair 
+lxi d,0040h   ;initializing DE pair
+mvi c,0fh     ;Moving 0fh to c
+start: mov a,m  ; moving content at memory address 0030h (which is stored in HL pair) to acc
+stax d          ;contents of the accumulator are copied into the memory location which is stored in DE pair
+inx d           ;increment in address (which is store in DE)
+inx h           ;increment in address (which is store in HL)
+dcr c           ;Decrementing counter value
+jnz start       ;jump if counter is not zero to "start"
 hlt
