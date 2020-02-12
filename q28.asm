94f09046c52d9a071ef09dcff60b428fd6374ed7
Aniket Raj <aniketraj.go@gmail.com>
2020-02-12 23:54:46 +0530

    set port c7 & c3 nd rset them aftr 0.2ms usng 8255

--- q28.asm
+++ q28.asm
@@ -1,17 +1,17 @@
 ;Write a program to set port c7 and c3 and reset them after 0.2ms
 ;use bsr mode of ppi 8255
-lxi sp,100
-mvi a,0fh
-out 83h
-mvi a,07h
-out 83h
+lxi sp,100 ; initalizing stack poointer with 100
+mvi a,0fh  ; moving 0f (control word) into accumulator
+out 83h    ; executed control word
+mvi a,07h  ; moving 07 into acc
+out 83h    
 call delay
 mvi a,06h
 out 83h
 mvi a,0eh
 out 83h
 hlt
-delay: lxi h,18
+delay: lxi h,18  ; defining delay subroutine 
 l1: dcx h
 mov a,l
 ora h
