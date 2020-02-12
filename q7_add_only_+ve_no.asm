93ef50383dd3b5459774510460fb779846246a0f
Aniket Raj <aniketraj.go@gmail.com>
2020-02-12 23:44:21 +0530

    To add only positive number out of 10 numbers

--- q7_add_only_+ve_no.asm
+++ q7_add_only_+ve_no.asm
@@ -5,11 +5,11 @@ mvi c,0fh ; intializing counter value
 mvi b,00h ; B will story carry 
 mvi d,00h
 mvi a,00h
-lxi h,0030h
-start: mov a,m
-ani 80h
-jz label
-jmp label1
+lxi h,0030h ; initializing HL pair with 0030h
+start: mov a,m   ;moving pointer content in to acc
+ani 80h   ; and operation between 80h and acc
+jz label  ; if zero jump to label
+jmp label1  ; jump to label1
 label: mov a,m
 add d
 mov d,a
@@ -18,9 +18,9 @@ inr b
 label1: inx h
 dcr c
 jz label2
-jmp start
-label2: mov a,d
-sta 0040h
-mov a,b
-sta 0041h
+jmp start    ; jump to start 
+label2: mov a,d ;moving final result into acc
+sta 0040h   ; storing acc content on to mem locn 0040h
+mov a,b    ;moving carry into acc
+sta 0041h   ; saving acc content into memory
 hlt 
