5759849b7a0e874af8965e06c9fa55ac0be51ce3
Aniket Raj <aniketraj.go@gmail.com>
2020-02-12 23:57:19 +0530

    Program to generate pulse of 2Mhz using 8254

--- q27.asm
+++ q27.asm
@@ -1,7 +1,7 @@
 ;program to generate pulse of 2Mhz using 8254
 ;using mode 2 of 8254
-mvi a,14h
-out 83h
-mvi a,64h
-out 80h
+mvi a,14h  ; moving appropriate contol word 
+out 83h    ; excuted control word
+mvi a,64h  ; =>for 2MHz
+out 80h    ; Out through port A
 hlt
