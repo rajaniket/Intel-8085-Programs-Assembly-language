d3c2e03e81f6de42741a14ad8bfb74df2d8050e3
Aniket Raj <aniketraj.go@gmail.com>
2020-02-12 23:50:15 +0530

    add 10 bytes of data stored in memory

--- q6_add_of_10_bytes.asm
+++ q6_add_of_10_bytes.asm
@@ -2,16 +2,16 @@
 ;store the result in 0040 and addtion carries in 0041
 
 mvi c,0Ah ;intializing counter value
-mvi a,00h
-mvi b,00h ;for carry
-lxi h,0030h
-begin: add m
-jnc label
-inr b
-label: inx h
-dcr c
-jnz begin
-sta 0040h
+mvi a,00h 
+mvi b,00h ; B for storing carry
+lxi h,0030h ; initialzing HL pair with 0030h
+begin: add m  ;add pointer with acc content
+jnc label     ;jump not carry to label
+inr b    ; increament b content
+label: inx h   ; increament hl pair 
+dcr c  ; decreament c 
+jnz begin   ; jump if not zero to begin
+sta 0040h  ; store addition result in 0040h
 mov a,b
-sta 0041h
+sta 0041h  ; store carry on to 0041h
 hlt
