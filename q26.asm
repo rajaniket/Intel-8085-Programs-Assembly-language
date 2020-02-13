dc25c0215ba340f9eae95675223e8239a3cfbe5f
Aniket Raj <aniketraj.go@gmail.com>
2020-02-13 23:52:22 +0530

    program to genrate square wave of 1khz using 8254

--- q26.asm
+++ q26.asm
@@ -1,9 +1,9 @@
 ;program to genrate square wave of 1khz using 8254
 ;use counter 1 with adrdress 81h
-mvi a,76h
-out 83h
-mvi a,208 ;d0
-out 81h
+mvi a,76h  ;moving control word into acc
+out 83h    ;Executing control word into control registor
+mvi a,208 ;d0 
+out 81h   ; out through counter one
 mvi a,07h
 out 81h
 hlt
