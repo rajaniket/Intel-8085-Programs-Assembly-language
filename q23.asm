a2df79c505de398c2b0943ba2330d8b69c7ac6e4
Aniket Raj <aniketraj.go@gmail.com>
2020-02-13 23:57:36 +0530

    add content of portA & B show its result in port c

--- q23.asm
+++ q23.asm
@@ -1,16 +1,16 @@
 ;prog to add content of portA and PortB
 ;show its result in port c
 ;use i/o mode of 8255
-lxi sp,100
-mvi a,92h
-out 83h
-in 80
-mov b,a
-in 81
-add b
-out 82
+lxi sp,100 ;initializing stack pointer 
+mvi a,92h  ;moving control word
+out 83h    ; executing control word into control register 
+in 80      ; taking input fron port A
+mov b,a    ; moving input data to registor B
+in 81      ; taking input fron port B
+add b      ; a=a+b
+out 82     ;addition result sending in port C
 hlt
-delay: lxi h,18
+delay: lxi h,18   
 l1: dcx h
 mov a,l
 ora h
