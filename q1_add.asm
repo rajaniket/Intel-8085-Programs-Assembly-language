090bdebe6bda99719615d0f44fbe44a862709f22
Aniket Raj <aniketraj.go@gmail.com>
2020-02-17 23:45:56 +0530

    Addition of two number

--- q1_add.asm
+++ q1_add.asm
@@ -2,9 +2,9 @@
 ;the numbers are stored in memory location 000AH and 000BH and save the result in 000CH
 LXI H,000AH ;MOVING MEMORY ADDRESS TO HL PAIR
 MOV A,M ; MOVING CONTENT AT 000AH TO ACC
-INX H
-ADD M
-STA 000CH
+INX H ; increament in hl pair
+ADD M ; adding pointer value to acc
+STA 000CH ;storing in memory
 HLT
 
 
